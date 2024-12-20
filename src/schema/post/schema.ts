99387f0a6943c1cfaa9237b4e3c2bf2204b00e81
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-12-21 01:34:37 +0300

    Add zod package and validation schema for Post create and update requests

--- src/schema/post/schema.ts
+++ src/schema/post/schema.ts
@@ -1,3 +1,38 @@
+import { z } from "zod";
+
+// Zod Schema
+const id = z.string().uuid();
+const title = z.string().min(4).max(255);
+const img_url = z.string().url();
+const slug = z.string().min(4).max(255);
+const content = z.string().min(100);
+const author_id = z.string().uuid();
+const category_id = z.number().int().positive();
+const tags = z.array(z.number().int().positive());
+const published = z.boolean().default(true);
+
+export const CreatePostSchema = z.object({
+  title,
+  img_url: img_url.optional(),
+  content,
+  author_id,
+  category_id,
+  tags: tags.optional(),
+  published,
+});
+
+export const UpdatePostSchema = z.object({
+  id,
+  title: title.optional(),
+  img_url: img_url.optional(),
+  content: content.optional(),
+  author_id: author_id.optional(),
+  category_id: category_id.optional(),
+  tags: tags.optional(),
+  published: published.optional(),
+});
+
+// GraphQL Schema
 export const postSchema = `#graphql
 type Post {
   id: ID!,
