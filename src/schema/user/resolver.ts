b72afdf05ace3453ad7b04057e45ad838c1469a4
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-12-21 16:43:32 +0300

    Add user creation, update, and deletion mutations with validation schemas

--- src/schema/user/resolver.ts
+++ src/schema/user/resolver.ts
@@ -1,7 +1,24 @@
-import { eq } from "drizzle-orm";
-import type { IResolverObject } from "mercurius";
+import { and, eq, name, or } from "drizzle-orm";
+import type { IResolverObject, MercuriusContext } from "mercurius";
 
 import { comments, likes, posts, users } from "@/config/db/schema.js";
+import {
+  CreateUserSchema,
+  DeleteUserSchema,
+  UpdateUserSchema,
+} from "./schema.js";
+import argon2 from "argon2";
+
+const returnFields = {
+  id: users.id,
+  name: users.name,
+  username: users.username,
+  email: users.email,
+  bio: users.bio,
+  profile_img: users.profile_img,
+  created_at: users.created_at,
+  updated_at: users.updated_at,
+};
 
 export const userQueries: IResolverObject<{ id: string | number }> = {
   Query: {
@@ -35,12 +52,14 @@ export const userResolvers: IResolverObject<{ id: string | number }> = {
       ctx.database
         .select()
         .from(posts)
-        .where(eq(posts.author_id, String(id))),
+        .where(and(eq(posts.author_id, String(id)), eq(posts.deleted, false))),
     comments: async ({ id }, _, ctx) =>
       ctx.database
         .select()
         .from(comments)
-        .where(eq(comments.author_id, String(id))),
+        .where(
+          and(eq(comments.author_id, String(id)), eq(comments.deleted, false)),
+        ),
     likes: async ({ id }, _, ctx) =>
       ctx.database
         .select()
@@ -48,4 +67,94 @@ export const userResolvers: IResolverObject<{ id: string | number }> = {
         .innerJoin(posts, eq(likes.post_id, posts.id))
         .where(eq(likes.user_id, String(id))),
   },
+  Mutations: {
+    createUser: async (_, data, ctx) => {
+      const { username, email, password } =
+        await CreateUserSchema.parseAsync(data);
+
+      const is_exist = await ctx.database
+        .select({ email: users.email, username: users.username })
+        .from(users)
+        .where(or(eq(users.username, username), eq(users.email, email)));
+
+      if (is_exist.length > 0) {
+        throw new Error("User already exists");
+      }
+
+      const password_hash = await argon2.hash(password);
+
+      const user = await ctx.database
+        .insert(users)
+        .values({ username, email, password_hash })
+        .returning(returnFields);
+
+      if (!user) {
+        throw new Error("Failed to create user");
+      }
+
+      return user;
+    },
+    updateUser: async (_, data, ctx) => {
+      const { id, current_password, ...user_data } =
+        await UpdateUserSchema.parseAsync(data);
+
+      await validateUserPassword(ctx, id, current_password);
+
+      const updated_user = await ctx.database
+        .update(users)
+        .set(user_data)
+        .where(eq(users.id, id))
+        .returning(returnFields);
+
+      if (!updated_user) {
+        throw new Error("Failed to update user");
+      }
+
+      return updated_user;
+    },
+    deleteUser: async (_, data, ctx) => {
+      const { id, password } = await DeleteUserSchema.parseAsync(data);
+
+      await validateUserPassword(ctx, id, password);
+
+      const user = await ctx.database
+        .delete(users)
+        .where(eq(users.id, id))
+        .returning(returnFields);
+
+      if (!user) {
+        throw new Error("Failed to delete user");
+      }
+
+      return user;
+    },
+  },
 };
+
+async function validateUserPassword(
+  ctx: MercuriusContext,
+  id: string,
+  current_password: string,
+) {
+  const user = await ctx.database
+    .select({ password_hash: users.password_hash })
+    .from(users)
+    .where(eq(users.id, id));
+
+  if (!user || user.length === 0) {
+    throw new Error("User does not exist");
+  }
+
+  if (user.length > 1) {
+    throw new Error("Multiple users with the same ID");
+  }
+
+  const is_password_validated = await argon2.verify(
+    user[0].password_hash,
+    current_password,
+  );
+
+  if (!is_password_validated) {
+    throw new Error("Invalid password");
+  }
+}
--- src/schema/user/schema.ts
+++ src/schema/user/schema.ts
@@ -1,14 +1,71 @@
+import { z } from "zod";
+
+// Zod Schema
+const id = z.string().uuid();
+const name = z
+  .string()
+  .min(3, { message: "Username must be at least 3 characters long" })
+  .max(255, { message: "Username must be less than 255 characters long" });
+const username = z
+  .string()
+  .min(3, { message: "Username must be at least 3 characters long" })
+  .max(255, { message: "Username must be less than 255 characters long" });
+const email = z.string().email();
+const password = z
+  .string()
+  .min(8, { message: "Password must be at least 8 characters long" })
+  .max(255, { message: "Password must be less than 255 characters long" })
+  .refine((p) => /[A-Z]/.test(p), {
+    message: "Password must contain an uppercase letter",
+  })
+  .refine((p) => /[a-z]/.test(p), {
+    message: "Password must contain a lowercase letter",
+  })
+  .refine((p) => /[0-9]/.test(p), {
+    message: "Password must contain a number",
+  })
+  .refine((p) => /[!@#$%^&*]/.test(p), {
+    message: "Password must contain a special character",
+  });
+const bio = z
+  .string()
+  .max(255, { message: "Bio must be less than 255 characters long" });
+const profile_img = z.string().url();
+
+export const CreateUserSchema = z.object({
+  username,
+  email,
+  password,
+});
+
+export const UpdateUserSchema = z.object({
+  id,
+  current_password: password,
+  name: name.optional(),
+  username: username.optional(),
+  email: email.optional(),
+  password: password.optional(),
+  bio: bio.optional(),
+  profile_img: profile_img.optional(),
+});
+
+export const DeleteUserSchema = z.object({
+  id,
+  password,
+});
+
+// GraphQL Schema
 export const userSchema = `#graphql
 type User {
   id: ID!,
+  name: String!,
   username: String!,
   email: String!,
-  password_hash: String!,
   bio: String,
+  profile_img: String!,
   posts: [Post!]!,
   comments: [Comment!],
   likes: [Post!]!,
-  profile_img: String!,
   created_at: String!,
   updated_at: String!,
 }`;
