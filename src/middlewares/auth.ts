ebf39622a3a55fc9148b4941c71aae6d7d8b2853
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-12-27 02:03:38 +0300

    Fix auth middleware to prevent blocking access

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,18 +2,22 @@ import fp from "fastify-plugin";
 import fastifyJwt from "@fastify/jwt";
 
 export const auth = fp(async function authHook(fastify, options) {
-  const { JWT_SECRET } = fastify.getEnvs() as { JWT_SECRET: string | undefined };
+  const { JWT_SECRET } = fastify.getEnvs() as {
+    JWT_SECRET: string | undefined;
+  };
 
   if (!JWT_SECRET) {
     throw Error("JWT_SECRET enviroment haven't set.");
   }
 
   await fastify.register(fastifyJwt, { secret: JWT_SECRET });
+
   fastify.addHook("onRequest", async (request, reply) => {
-    try {
-      await request.jwtVerify();
-    } catch (err) {
-      reply.send(err);
+    const auth = request.headers.authorization;
+    if (!auth) {
+      return;
     }
+
+    request.user = await request.jwtVerify();
   });
 });
